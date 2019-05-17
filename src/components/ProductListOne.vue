51ac0a900405a9a2381328a84ab9611d429b354c
winnee0solta <winneecreztha@gmail.com>
2019-05-17 11:36:49 +0545

    added button on product list one component

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -7,6 +7,7 @@
  <span class="price">${{product.price}}</span>
  </li>
  </ul>
+ <button v-on:click="reducePrice">Reduce Price</button>
  </div>
 </template>
 
@@ -20,6 +21,13 @@ export default {
   saleProducts(){
     return this.$store.getters.saleProducts
     //
+  }
+},
+methods:{
+  reducePrice:function(){
+    this.$store.state.products.forEach(product=>{
+      product.price -= 1
+    })
   }
 }
 
